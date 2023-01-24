6a1a83e2717c6a0c9b948e0430c1ae1c26100611
Cordione <52832668+Cordione@users.noreply.github.com>
2023-01-24 17:12:59 +0100

    clean up- spelling

--- sumOfWords.ts
+++ sumOfWords.ts
@@ -1,11 +1,11 @@
 import { countWords } from "./countWords";
 
-export function sumOfWords(input: string){
-// Pass input to count words
-const length = countWords(input)
-// Split by . exclude empty string
-const amountOfSentences = input.split(".").filter(x => x != '')
-//divide length by amountOfSentences
-const output = Math.ceil(length/amountOfSentences.length)
-return output
+export function sumOfWords(input: string) {
+  // Pass input to count words
+  const length = countWords(input);
+  // Split by . exclude empty string
+  const amountOfSentences = input.split(".").filter((x) => x != "");
+  //divide length by amountOfSentences
+  const output = Math.ceil(length / amountOfSentences.length);
+  return output;
 }

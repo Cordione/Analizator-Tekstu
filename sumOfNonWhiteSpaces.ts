46b0a438cc534ea1f88fc32739b3f9d99bc5fd3e
Cordione <52832668+Cordione@users.noreply.github.com>
2023-01-24 19:56:01 +0100

    formating change

--- sumOfNonWhiteSpaces.ts
+++ sumOfNonWhiteSpaces.ts
@@ -1,11 +1,11 @@
 import { countWhiteSpaces } from "./countWhiteSpaces";
 
-export function sumOfNonWhiteSpaces(input: string){
-// Pass input to count non white spaces
-const length = countWhiteSpaces(input, false)
-// Split by . exclude empty string
-const amountOfSentences = input.split(".").filter(x => x != '')
-//divide length by amountOfSentences
-const output = Math.ceil(length/amountOfSentences.length)
-return output
+export function sumOfNonWhiteSpaces(input: string) {
+  // Pass input to count non white spaces
+  const length = countWhiteSpaces(input, false);
+  // Split by . exclude empty string
+  const amountOfSentences = input.split(".").filter((x) => x != "");
+  //divide length by amountOfSentences
+  const output = Math.ceil(length / amountOfSentences.length);
+  return output;
 }

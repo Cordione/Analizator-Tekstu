aaeec7f3fbc17d374fb779b5d440690c7b9a905a
Cordione <52832668+Cordione@users.noreply.github.com>
2023-01-26 17:59:30 +0100

    updated comment

--- functions/shortestSentenceByWhiteSpaces.ts
+++ functions/shortestSentenceByWhiteSpaces.ts
@@ -7,7 +7,7 @@ export function shortestSentenceByWhiteSpaces(input: string, byWhiteSpaces: bool
   //Add variable to store length of shortest sentence, set it as undefined for now
   let length: undefined | number = undefined;
   //Then iterate throught inputSplit.length calling function countWord and update this 2 variables
-  //Verify if inputSplit is not empty
+  //Verify if inputSplit length is greater then 0
   if (inputSplit.length > 0) {
     for (let index = 0; index < inputSplit.length; index++) {
       const amountOfNonWhiteSigns = countWhiteSpaces(inputSplit[index], byWhiteSpaces);

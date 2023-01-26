728d0965a7735845a8d89ff7e4bffa1251d39d2b
Cordione <52832668+Cordione@users.noreply.github.com>
2023-01-26 18:34:50 +0100

    added verification if elements are not empty

--- functions/sumOfWords.ts
+++ functions/sumOfWords.ts
@@ -5,7 +5,12 @@ export function sumOfWords(input: string) {
   const length = countWords(input);
   // Split by . exclude empty string
   const amountOfSentences = input.split(".").filter((x) => x != "");
-  //divide length by amountOfSentences
-  const output = Math.ceil(length / amountOfSentences.length);
-  return output;
+  //Verify if length && amount of sentences != 0
+  if (length != 0 && amountOfSentences.length != 0) {
+    //divide length by amountOfSentences
+    const output = Math.ceil(length / amountOfSentences.length);
+    return output;
+  } else {
+    return 0;
+  }
 }

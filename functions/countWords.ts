b01dcd13794a9c1b525f22b42de9706ee9785167
Cordione <52832668+Cordione@users.noreply.github.com>
2023-01-26 20:20:27 +0100

    Added regex to properly count words(they must have at least one letter)

--- functions/countWords.ts
+++ functions/countWords.ts
@@ -1,5 +1,7 @@
 export function countWords(input: string): number {
   //Split input by space, exclude empty inputs and return length
-  const output = input.split(" ").filter((x) => x != "").length;
+  //Add regex to ensure that word contains at least one letter
+  const reg = /[A-Za-z]/
+  const output = input.split(" ").filter((x) => x != "" && x.match(reg)).length;
   return output;
 }

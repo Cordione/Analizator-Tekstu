d7191e918dbd69dde34981b1f8e3d24c9c196509
Cordione <52832668+Cordione@users.noreply.github.com>
2023-01-26 19:27:06 +0100

    added split by ? !

--- functions/sumOfWords.ts
+++ functions/sumOfWords.ts
@@ -3,8 +3,8 @@ import { countWords } from "./countWords";
 export function sumOfWords(input: string) {
   // Pass input to count words
   const length = countWords(input);
-  // Split by . exclude empty string
-  const amountOfSentences = input.split(".").filter((x) => x != "");
+  // Split by . ! ? exclude empty string
+  const amountOfSentences = input.split(". " && "! " && "? ").filter((x) => x != "");
   //Verify if length && amount of sentences != 0
   if (length != 0 && amountOfSentences.length != 0) {
     //divide length by amountOfSentences

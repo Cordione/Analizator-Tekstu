522a830fa87cdbc218113b06bb046e0ac9a1e090
Cordione <52832668+Cordione@users.noreply.github.com>
2023-02-01 16:27:12 +0100

    updated logic in countWords to match new conditions

--- functions/countWords.ts
+++ functions/countWords.ts
@@ -1,7 +1,8 @@
-export function countWords(input: string): number {
-  //Split input by space, exclude empty inputs and return length
-  //Add regex to ensure that word contains at least one letter
-  const reg = /[a-zA-Z]/;
-  const output = input.split(" ").filter((x) => x != "" && x.match(reg)).length;
-  return output;
+export function countWords(sentence: string): number {
+  const haveAtLeastOneLetter = /[A-Za-z]/;
+  const haveSpecialCharacters = /[$&+:;=@#|'<>^*()%-]/;
+  //Split sentence by Space
+  const splitedSentence = sentence.split(" ").filter((x) => x.match(haveAtLeastOneLetter) && !x.match(haveSpecialCharacters));
+
+  return splitedSentence.length;
 }
